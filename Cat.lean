abb76403b1a8af1b5e51a639cc78f70adfce558e
Soham Saha <127578105+Soham-Saha@users.noreply.github.com>
2026-01-19 19:56:50 +0530

    Initial Lean commit

---
+++ Cat.lean
@@ -0,0 +1,80 @@
+-- Do we want (a :->: b) to be one Sort higher than a,b?
+-- Do we want a,b to be of different Sorts?
+axiom hom.{u} {x y : Sort u} : x -> y -> Sort u
+
+infixr : 60 " :->: " => hom
+
+axiom comp.{u} {x y z : Sort u} : x :->: y -> y :->: z -> x :->: z
+
+infixl : 70 " >-> " => comp
+
+axiom ident.{u} {x : Sort u} : x :->: x
+
+axiom comp_id_left.{u}  {x y : Sort u} (f : x :->: y) : ident >-> f = f
+
+axiom comp_id_right.{u} {x y : Sort u} (f : x :->: y) : f >-> ident = f
+
+---------------------------------------------------------------------------------------
+
+-- Do we want product of a b to be at the same Sort as a b?
+-- Do we want a b to be of different Sorts?
+axiom prod.{u} {x y : Sort (u+1)} : x -> y -> Sort u
+
+infixl : 67 " :><: " => prod
+
+axiom func_pair.{u} {x a b : Sort u} : x :->: a -> x :->: b -> x :->: a :><: b
+
+infixl : 67 " <> " => func_pair
+
+axiom fst.{u} {a b : Sort u} : a :><: b :->: a
+
+axiom snd.{u} {a b : Sort u} : a :><: b :->: b
+
+axiom proj_first.{u}  {x a b : Sort u} (f : x :->: a) (g : x :->: b)
+  : (f <> g) >-> fst = f
+
+axiom proj_second.{u} {x a b : Sort u} (f : x :->: a) (g : x :->: b)
+  : (f <> g) >-> snd = g
+
+noncomputable def func_prod.{u} {x1 y1 x2 y2 : Sort u}
+  (f : x1 :->: y1) (g : x2 :->: y2) : x1 :><: x2 :->: y1 :><: y2 :=
+    fst >-> f <> snd >-> g
+
+infixl : 67 " >< " => func_prod
+
+---------------------------------------------------------------------------------------
+
+-- Do we want a^b to be at the same Sort as a b?
+-- Do we want a b to be of different Sorts?
+axiom exp.{u} {x y : Sort (u+1)} : x -> y -> Sort u
+
+infixl : 68 " :^: " => exp
+
+axiom curry.{u} {x a b : Sort u} : x :><: a :->: b -> x :->: b :^: a
+
+axiom evaluate.{u} {a b : Sort u} : (b :^: a :><: a) :->: b
+
+-- This says that the curried and uncurried versions of f act in the
+-- same way on the arguments.
+axiom curry_axiom.{u} {x a b : Sort u} {f: x :><: a :->: b}
+  : (curry f >< ident) >-> evaluate = f
+
+noncomputable def isom0to1.{u} {a b c : Sort u}
+  : (a :><: b) :^: c :->: (a :^: c :><: b :^: c) :=
+    curry (evaluate >-> fst) <> curry (evaluate >-> snd)
+
+noncomputable def isom1to0.{u} {a b c : Sort u}
+  : (a :^: c :><: b :^: c) :->: (a :><: b) :^: c :=
+    curry ((fst >< ident) >-> evaluate <> (snd >< ident) >-> evaluate)
+
+theorem isom_indeed_1.{u} {a b c : Sort u}
+  : (isom0to1 : (a :><: b) :^: c :->: _) >-> isom1to0 = ident := by
+    unfold isom0to1 isom1to0
+
+    sorry
+
+theorem isom_indeed_2.{u} {a b c : Sort u}
+  : isom1to0 >-> (isom0to1 : (a :><: b) :^: c :->: _) = ident := by
+    unfold isom0to1 isom1to0
+
+    sorry
